4eeffa2f1d1e8c38275197d665643e60bba74346
rtsmc <ryan@trubey.net>
2025-08-01 23:35:36 -0700

    add screen bounds & small movement qol

--- src/player.c
+++ src/player.c
@@ -3,21 +3,35 @@
 #include <raylib.h>
 #include <raymath.h>
 
+static bool MouseOnPlayer(Sprite *self) {
+    return GetMouseX() > self->destRec.x - self->destRec.width / 2 &&
+           GetMouseX() < self->destRec.x + self->destRec.width / 2 &&
+           GetMouseY() > self->destRec.y - self->destRec.height / 2 &&
+           GetMouseY() < self->destRec.y + self->destRec.height / 2;
+}
+
 static void PlayerUpdate(Sprite *self) {
     Player *p = (Player *)self;
 
-    if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON)) {
-        p->dest = (Vector2){GetMouseX(), GetMouseY()};
+    if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON) && !MouseOnPlayer(self)) {
+        float w = self->destRec.width;
+        float h = self->destRec.height;
+
+        p->dest =
+            (Vector2){Clamp(GetMouseX(), 0 + w / 2, GetScreenWidth() - w / 2),
+                      Clamp(GetMouseY(), 0 + h / 2, GetScreenHeight() - h / 2)};
         p->hasDest = true;
 
         Vector2 delta = (Vector2){p->dest.x - p->base.destRec.x,
-            p->dest.y - p->base.destRec.y};
-        p->base.vel = Vector2Scale(delta, p->base.moveSpeed/Vector2Length(delta));
+                                  p->dest.y - p->base.destRec.y};
+        p->base.vel =
+            Vector2Scale(delta, p->base.moveSpeed / Vector2Length(delta));
     }
 
     if (p->hasDest) {
-        Vector2 dist = (Vector2){p->dest.x - p->base.destRec.x, p->dest.y - p->base.destRec.y};
-        if (Vector2LengthSqr(dist) < 10) {
+        Vector2 dist = (Vector2){p->dest.x - p->base.destRec.x,
+                                 p->dest.y - p->base.destRec.y};
+        if (Vector2LengthSqr(dist) < 0.5f) {
             p->base.vel = Vector2Zero();
             p->base.destRec.x = p->dest.x;
             p->base.destRec.y = p->dest.y;
